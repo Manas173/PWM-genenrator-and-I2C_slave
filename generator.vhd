2999daa973e7cf9e26713325f0c1215446c99f51
Manas173 <31970009+Manas173@users.noreply.github.com>
2018-01-25 18:29:24 +0530

    Update generator.vhd

--- generator.vhd
+++ generator.vhd
@@ -1,34 +1,35 @@
---Author: Manas Ranjan Swain
+
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
-use IEEE.STD_LOGIC_arith.ALL;
-use IEEE.STD_LOGIC_unsigned.ALL;
+use IEEE.STD_LOGIC_ARITH.all;
+use IEEE.STD_LOGIC_UNSIGNED.all;
 
-entity generator is
-port(duty_cycle:in STD_LOGIC_VECTOR(6 downto 0);
-		clk:in STD_LOGIC;
-		pwm:out STD_LOGIC);
-end generator;
+entity pwm is
+generic(N: INTEGER :=8);
+port(clk:in STD_LOGIC;
+		pwm_count:in STD_LOGIC_VECTOR(N-1 downto 0);
+		pwm_out:out STD_LOGIC);
 
-architecture Behavioral of generator is
-shared variable count:STD_LOGIC_VECTOR(6 downto 0):=(others=>'0');
-signal dc: STD_LOGIC_VECTOR(6 downto 0);
+end pwm;
+
+architecture Behavioral of pwm is
+signal max_count:STD_LOGIC_VECTOR(N-1 downto 0):=(others=>'1');
+signal cout:STD_LOGIC_VECTOR(N-1 downto 0):=(others=>'0');
 begin
-dc<=duty_cycle;
 process(clk)
 begin
-if(clk'event and clk='1') then
-	if(count<"1100000")then
-		if(count<dc) then
-			pwm<='1';
+	if(clk'event and clk='1')then
+		if(cout <= max_count) then
+			if(cout <= pwm_count)then
+				pwm_out <= '1';
+			else
+				pwm_out <= '0';
+			end if;
+			cout<=cout + 1;
 		else
-			pwm<='0';
+			cout<=(others=>'0');
 		end if;
-		count:=count+1;
-	else
-		count:="0000000";
 	end if;
-	end if;
-	end process;
+end process;
 end Behavioral;
 

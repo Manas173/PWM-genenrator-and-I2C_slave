1e119422c4290e62a7535cade144fd407a052392
Manas173 <31970009+Manas173@users.noreply.github.com>
2018-01-27 13:12:15 +0530

    Update i2c_slave_pwm_control.vhd

--- i2c_slave_pwm_control.vhd
+++ i2c_slave_pwm_control.vhd
@@ -98,6 +98,49 @@ begin
 end process;
 end I2C;
 
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.STD_LOGIC_ARITH.all;
+use IEEE.STD_LOGIC_UNSIGNED.all;
+
+entity pwm is
+generic(N: INTEGER);
+port(clk:in STD_LOGIC;
+		pwm_count:in STD_LOGIC_VECTOR(N-1 downto 0);
+		pwm_out:out STD_LOGIC);
+
+end pwm;
+
+architecture Behavioral of pwm is
+signal max_count:STD_LOGIC_VECTOR(N-1 downto 0):=(others=>'1');
+signal cout:STD_LOGIC_VECTOR(N-1 downto 0):=(others=>'0');
+signal pwm_input:STD_LOGIC_VECTOR(N-1 downto 0);
+signal check:STD_LOGIC_VECTOR(N-1 downto 0):=(others=>'Z');
+begin
+process(clk)
+begin
+if(pwm_count=check) then
+	pwm_input<=(others=>'0');
+else
+	pwm_input<=pwm_count;
+end if;
+	if(clk'event and clk='1')then
+		if(cout <= max_count) then
+			if(cout < pwm_input)then
+				pwm_out <= '1';
+			else
+				pwm_out <= '0';
+			end if;
+			cout<=cout + 1;
+		else
+			cout<=(others=>'0');
+		end if;
+	end if;
+end process;
+end Behavioral;
+
+
+
 
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
@@ -172,46 +215,4 @@ pwm_gen : pwm
 		pwm_count => dat ,
 		pwm_out => pwm_out);
 
-end combined;
-
-
-library IEEE;
-use IEEE.STD_LOGIC_1164.ALL;
-use IEEE.STD_LOGIC_ARITH.all;
-use IEEE.STD_LOGIC_UNSIGNED.all;
-
-entity pwm is
-generic(N: INTEGER);
-port(clk:in STD_LOGIC;
-		pwm_count:in STD_LOGIC_VECTOR(N-1 downto 0);
-		pwm_out:out STD_LOGIC);
-
-end pwm;
-
-architecture Behavioral of pwm is
-signal max_count:STD_LOGIC_VECTOR(N-1 downto 0):=(others=>'1');
-signal cout:STD_LOGIC_VECTOR(N-1 downto 0):=(others=>'0');
-signal pwm_input:STD_LOGIC_VECTOR(N-1 downto 0);
-signal check:STD_LOGIC_VECTOR(N-1 downto 0):=(others=>'Z');
-begin
-process(clk)
-begin
-if(pwm_count=check) then
-	pwm_input<=(others=>'0');
-else
-	pwm_input<=pwm_count;
-end if;
-	if(clk'event and clk='1')then
-		if(cout <= max_count) then
-			if(cout < pwm_input)then
-				pwm_out <= '1';
-			else
-				pwm_out <= '0';
-			end if;
-			cout<=cout + 1;
-		else
-			cout<=(others=>'0');
-		end if;
-	end if;
-end process;
-end Behavioral;
+end architecture combined;

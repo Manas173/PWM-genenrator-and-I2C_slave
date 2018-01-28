a2840c32670e67b7783f78268b90e7d98727fdb5
Manas173 <31970009+Manas173@users.noreply.github.com>
2018-01-28 21:18:25 +0530

    Update i2c_slave_pwm_control.vhd

--- i2c_slave_pwm_control.vhd
+++ i2c_slave_pwm_control.vhd
@@ -1,223 +1,247 @@
-library IEEE;
-use IEEE.STD_LOGIC_1164.ALL;
-use IEEE.std_logic_arith.all;
-use IEEE.std_logic_unsigned.all;
+LIBRARY IEEE;
+USE IEEE.STD_LOGIC_1164.ALL;
+USE IEEE.std_logic_arith.ALL;
+USE IEEE.std_logic_unsigned.ALL;
 
-entity slave is
-	generic(address: STD_LOGIC_VECTOR(6 downto 0);	--Seven bit address 
-		N: INTEGER);				--N define the data bits size
+ENTITY slave IS
+	GENERIC (
+		address : STD_LOGIC_VECTOR(6 DOWNTO 0); --Seven bit address
+	N : INTEGER); 					--N define the data bits size
 
-	port(	clk:in std_logic;			--system clock
-		sda:inout std_logic:='Z';
-		reset:in std_logic;			--goes to idle state when reset is 1
-		scl:in std_logic;		
-		d:out STD_LOGIC_VECTOR(N-1 downto 0));	--contains data to be transmitted
-end slave;
+	PORT (
+		clk : IN std_logic; 			--system clock
+		sda : INOUT std_logic := 'Z';
+		reset : IN std_logic;			--goes to idle state when reset is 1
+		scl : IN std_logic; 
+	d : OUT STD_LOGIC_VECTOR(N - 1 DOWNTO 0)); 	--contains data to be transmitted
+END slave;
 
-architecture I2C of slave is
-	type machine is (idle,start,read_address,receive_data);	
-	signal state:machine :=idle;
-	signal data:STD_LOGIC_VECTOR(N-1 downto 0):=(others=>'0');
-	signal addr:STD_LOGIC_VECTOR(6 downto 0):=(others=>'0');
-	shared variable count:INTEGER range N-1 downto 0:=0;
-	shared variable flag:STD_LOGIC:='0';
+ARCHITECTURE I2C OF slave IS
+	TYPE machine IS (idle, start, read_address, receive_data); 
+	SIGNAL state : machine := idle;
+	SIGNAL data : STD_LOGIC_VECTOR(N - 1 DOWNTO 0) := (OTHERS => '0');
+	SIGNAL addr : STD_LOGIC_VECTOR(6 DOWNTO 0) := (OTHERS => '0');
+	SHARED VARIABLE count : INTEGER RANGE N - 1 DOWNTO 0 := 0;
+	SHARED VARIABLE flag : STD_LOGIC := '0';
 
-begin
+BEGIN
+	PROCESS (clk, reset, scl)
+	BEGIN
+		IF rising_edge(clk) THEN
+			IF reset = '0' THEN
+				CASE state IS
+					WHEN idle => 
+						IF clk'EVENT AND clk = '1' THEN
+						END IF;
+						IF (scl = '1' AND falling_edge(sda)) THEN
+							state <= start;
+							sda <= 'Z';
+						END IF;
+					WHEN start => 
+						IF rising_edge(clk) THEN
+							state <= read_address;
+							count := 6;
+						END IF;
+					WHEN read_address => -- Reads the address from the SDA line
+						IF count >= 0 THEN
+							IF rising_edge(scl) THEN
+								addr(count) <= sda;
+								count := count - 1;
+							END IF;
+						ELSE
+							IF addr = address THEN
+								IF falling_edge(scl) THEN
+									sda <= '0';
+									flag := '1';
+								END IF;
+							ELSE
+								sda <= 'Z';
+								flag := '0';
+							END IF;
+ 
+							IF (flag = '1' AND rising_edge(scl)) THEN
+								state <= receive_data;
+								count := N - 1;
+								d <= (OTHERS => '0');
+								data <= (OTHERS => '0');
+							ELSIF (flag = '0' AND falling_edge(scl)) THEN
+								state <= idle;
+								sda <= 'Z';
+								d <= (OTHERS => 'Z');
+							END IF;
+						END IF;
+					WHEN receive_data => --Goes to receive data if the address matches
+						IF count >= 0 THEN
+							IF rising_edge(scl) THEN
+								data(count) <= sda;
+								count := count - 1;
+							END IF;
+						ELSE
+							IF falling_edge(scl) THEN
+								sda <= '0';
+								flag := '1';
+								d(N - 1 DOWNTO 0) <= data;
+								count := N - 1;
+							END IF;
+						END IF;
+ 
+						--STOP condition 
+						IF (flag = '1' AND scl = '1' AND rising_edge(sda)) THEN 
+							state <= idle; 
+							flag := '0';
+							d(N - 1 DOWNTO 0) <= (OTHERS => '0');
+						END IF;
+				END CASE;
+ 
+			ELSE
+				state <= idle; -- goes here when the reset is 1
+				flag := '0';
+				d(N - 1 DOWNTO 0) <= (OTHERS => '0');
+ 
+			END IF;
+		END IF;
 
-	process(clk,reset,scl)
-	begin
-	if(clk'event and clk='1') then
-		if reset='0' then
-			case state is
-				when idle=>
-					if clk'event and clk='1' then
-					end if;
-					if(scl='1' and sda'event and sda='0') then
-						state<=start;
-						sda<='Z';
-					end if;
-				when start=>
-					if(clk'event and clk='1') then
-						state<=read_address;
-						count:=6;
-					end if;
-				when read_address=>			-- Reads the address from the SDA line 
-					if(count>=0) then 
-						if(scl'event and scl='1') then
-							addr(count)<=sda;
-							count:=count-1;
-						end if;
-					else
-						if addr=address then
-							if(scl'event and scl='0') then
-								sda<='Z';
-								flag:='1';
-							end if;
-						else
-							sda<='1';
-							flag:='0';
-						end if;
-					
-						if (flag='1' and scl'event and scl='1') then
-							state<=receive_data;
-							count:=N-1;
-							d<=(others=>'0');
-							data<=(others=>'0');
-						elsif(flag='0' and scl'event and scl='0') then
-								state<=idle;
-								sda<='Z';
-								d<=(others=>'Z');
-						end if;
-					end if;
-				when receive_data=>		--Goes to receive data if the address matches
-					if(count >= 0) then 
-						if(scl'event and scl='1') then
-							data(count)<=sda;
-							count:=count-1;
-						end if;
-					else
-						if(scl'event and scl='0') then
-							sda<='Z';
-							flag:='1';
-							d(N-1 downto 0)<=data;
-							count:=N-1;
-						end if;
-					end if;
-					
-					--STOP condition	
-					if(flag='1' and scl='1' and sda='1' and sda'event) then	  
-						state<=idle;					
-						flag:='0';
-						d(N-1 downto 0)<=(others=>'0');
-					end if;
-				end case;
-					
-		else
-			state<=idle;			-- goes here when the reset is 1
-			flag:='0';
-			d(N-1 downto 0)<=(others=>'0');
-			
-		end if;
-	end if;
+	END PROCESS;
+	END I2C;
 
-	end process;
-end I2C;
+	LIBRARY IEEE; 
+	USE IEEE.STD_LOGIC_1164.ALL;
+	USE IEEE.STD_LOGIC_ARITH.ALL;
+	USE IEEE.STD_LOGIC_UNSIGNED.ALL;
 
-library IEEE;						
-use IEEE.STD_LOGIC_1164.ALL;
-use IEEE.STD_LOGIC_ARITH.all;
-use IEEE.STD_LOGIC_UNSIGNED.all;
+	ENTITY pwm IS -- The pwm generator starts here
+		GENERIC (N : INTEGER); -- Defines the pwm data bit size
 
-entity pwm is						-- The pwm generator starts here
-	generic(N: INTEGER);				-- Defines the pwm data bit size
+		PORT (
+		clk : IN STD_LOGIC;
+		pwm_count : IN STD_LOGIC_VECTOR(N - 1 DOWNTO 0);
+		pwm_out : OUT STD_LOGIC
+		);
 
-	port(	clk:in STD_LOGIC;
-		pwm_count:in STD_LOGIC_VECTOR(N-1 downto 0);
-		pwm_out:out STD_LOGIC);
+END pwm;
 
-end pwm;
+ARCHITECTURE Behavioral OF pwm IS
+ 
+	SIGNAL max_count : STD_LOGIC_VECTOR(N - 1 DOWNTO 0) := (OTHERS => '1');
+		SIGNAL cout : STD_LOGIC_VECTOR(N - 1 DOWNTO 0) := (OTHERS => '0');
+			SIGNAL pwm_input : STD_LOGIC_VECTOR(N - 1 DOWNTO 0);
+			SIGNAL check : STD_LOGIC_VECTOR(N - 1 DOWNTO 0) := (OTHERS => 'Z');
+			BEGIN
+				PROCESS (clk)
+				BEGIN
+					IF (pwm_count = check) THEN
+						pwm_input <= (OTHERS => '0');
+					ELSE
+						pwm_input <= pwm_count;
+					END IF;
+					IF rising_edge(clk) THEN
+						IF cout <= max_count THEN
+							IF cout < pwm_input THEN
+								pwm_out <= '1';
+							ELSE
+								pwm_out <= '0';
+							END IF;
+							cout <= cout + 1;
+						ELSE
+							cout <= (OTHERS => '0');
+						END IF;
+					END IF;
+				END PROCESS;
+				END Behavioral;
 
-architecture Behavioral of pwm is
-	
-	signal max_count:STD_LOGIC_VECTOR(N-1 downto 0):=(others=>'1');
-	signal cout:STD_LOGIC_VECTOR(N-1 downto 0):=(others=>'0');
-	signal pwm_input:STD_LOGIC_VECTOR(N-1 downto 0);
-	signal check:STD_LOGIC_VECTOR(N-1 downto 0):=(others=>'Z');
-	begin
-		process(clk)
-		begin
-			if(pwm_count=check) then
-				pwm_input<=(others=>'0');
-			else
-				pwm_input<=pwm_count;
-			end if;
-			if(clk'event and clk='1')then
-				if(cout <= max_count) then
-					if(cout < pwm_input)then
-						pwm_out <= '1';
-					else
-						pwm_out <= '0';
-					end if;
-				cout<=cout + 1;
-				else
-		cout<=(others=>'0');
-		end if;
-	end if;
-end process;
-end Behavioral;
+				LIBRARY IEEE; --To make a combined entity of I2C slave and PWM controller
+				USE IEEE.STD_LOGIC_1164.ALL;
 
-library IEEE;			--To make a combined entity of I2C slave and PWM controller
-use IEEE.STD_LOGIC_1164.ALL;
+				ENTITY I2Cpwm IS
+					PORT (
+					clk : IN std_logic;
+					sda : INOUT std_logic := 'Z';
+					reset : IN std_logic;
+					scl : IN std_logic;
+					pwm_out : OUT STD_LOGIC
+					);
+				END I2Cpwm;
 
-entity I2Cpwm is
-	   port(clk:in std_logic;
-		sda:inout std_logic:='Z';
-		reset:in std_logic;
-		scl:in std_logic;
-		pwm_out:out STD_LOGIC);
-end I2Cpwm;
+				ARCHITECTURE Behavioral OF I2Cpwm IS
+					COMPONENT pwm
+						GENERIC (N : INTEGER);
+						PORT (
+							clk : IN std_logic;
+							pwm_count : IN std_logic_vector(7 DOWNTO 0); 
+							pwm_out : OUT std_logic
+						);
+					END COMPONENT;
+ 
+					COMPONENT slave
+						GENERIC (
+							address : STD_LOGIC_VECTOR(6 DOWNTO 0);
+							N : INTEGER
+						);
+						PORT (
+							clk : IN std_logic;
+							reset : IN std_logic;
+							scl : IN std_logic; 
+							sda : INOUT std_logic; 
+							d : OUT std_logic_vector(7 DOWNTO 0)
+						);
+					END COMPONENT;
+					SIGNAL dat : std_logic_vector(7 DOWNTO 0);
+				BEGIN
+					--Four I2C registers
+					--Data size is kept as 8 bits
+					i2c_reg1 : slave
+						GENERIC MAP(
+						address => "1010100", 
+						N => 8)
+						PORT MAP(
+							clk => clk, 
+							reset => reset, 
+							scl => scl, 
+							sda => sda, 
+							d => dat
+							);
+					i2c_reg2 : slave
+						GENERIC MAP(
+						address => "1010000", 
+						N => 8)
+						PORT MAP(
+							clk => clk, 
+							reset => reset, 
+							scl => scl, 
+							sda => sda, 
+							d => dat
+							);
+					i2c_reg3 : slave
+						GENERIC MAP(
+						address => "1010111", 
+						N => 8)
+						PORT MAP(
+							clk => clk, 
+							reset => reset, 
+							scl => scl, 
+							sda => sda, 
+							d => dat
+							);
+					i2c_reg4 : slave
+						GENERIC MAP(
+						address => "1111111", 
+						N => 8)
+						PORT MAP(
+							clk => clk, 
+							reset => reset, 
+							scl => scl, 
+							sda => sda, 
+							d => dat
+							); 
 
-architecture Behavioral of I2Cpwm is
-	COMPONENT pwm
-		generic(N: INTEGER);
-		PORT(clk : IN std_logic;
-		     pwm_count : IN std_logic_vector(7 downto 0);          
-		     pwm_out : OUT std_logic);
-	END COMPONENT; 
-	
-	COMPONENT slave
-	generic(address: STD_LOGIC_VECTOR(6 downto 0);
-		N:INTEGER);
-		PORT(
-			clk : IN std_logic;
-			reset : IN std_logic;
-			scl : IN std_logic;    
-			sda : INOUT std_logic;     
-			d : OUT std_logic_vector(7 downto 0));
-	END COMPONENT;
-	signal dat:std_logic_vector(7 downto 0);
-	begin
-		--Four I2C registers
-		--Data size is kept as 8 bits
-		i2c_reg1 : slave
-  		 generic map(address=>"1010100",
-					N=>8)
-			port map(clk =>clk,
-				reset => reset,
-				scl => scl,   
-				sda => sda,     
-				d => dat);
-		i2c_reg2 : slave
-   		 generic map(address=>"1010000",
-					N=>8)
-			port map(clk =>clk,
-				reset => reset,
-				scl => scl,   
-				sda => sda,     
-				d => dat);
-		i2c_reg3 : slave
-  		 generic map(address=>"1010111",
-					N=>8)
-			port map(clk =>clk,
-				reset => reset,
-				scl => scl,   
-				sda => sda,     
-				d => dat);
-		i2c_reg4 : slave
-  		 generic map(address=>"1111111",
-					N=>8)
-			port map(clk =>clk,
-				reset => reset,
-				scl => scl,   
-				sda => sda,     
-				d => dat);		
+					pwm_gen : pwm
+						GENERIC MAP(N => 8)
+						PORT MAP(
+							clk => clk, 
+							pwm_count => dat, 
+							pwm_out => pwm_out
+							);
+ 
+ 
 
-		pwm_gen : pwm
-		 generic map(N=> 8)
-			port map(clk => clk ,
-			pwm_count => dat ,
-			pwm_out => pwm_out);
-	
-	
-
-end Behavioral;
+END Behavioral;

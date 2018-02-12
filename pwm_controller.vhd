6dbd5d569721db25975bda05d2be063939d2686c
Manas173 <31970009+Manas173@users.noreply.github.com>
2018-02-12 21:49:36 +0530

    Update pwm_controller.vhd

--- pwm_controller.vhd
+++ pwm_controller.vhd
@@ -1,40 +1,42 @@
-library IEEE;
-use IEEE.STD_LOGIC_1164.ALL;
-use IEEE.STD_LOGIC_ARITH.all;
-use IEEE.STD_LOGIC_UNSIGNED.all;
-
-entity pwm is
-generic(N: INTEGER);
-port(clk:in STD_LOGIC;
-		pwm_count:in STD_LOGIC_VECTOR(N-1 downto 0);
-		pwm_out:out STD_LOGIC);
-
-end pwm;
-
-architecture Behavioral of pwm is
-signal max_count:STD_LOGIC_VECTOR(N-1 downto 0):=(others=>'1');
-signal cout:STD_LOGIC_VECTOR(N-1 downto 0):=(others=>'0');
-signal pwm_input:STD_LOGIC_VECTOR(N-1 downto 0);
-signal check:STD_LOGIC_VECTOR(N-1 downto 0):=(others=>'Z');
-begin
-process(clk)
-begin
-if(pwm_count=check) then
-	pwm_input<=(others=>'0');
-else
-	pwm_input<=pwm_count;
-end if;
-	if(clk'event and clk='1')then
-		if(cout <= max_count) then
-			if(cout < pwm_input)then
-				pwm_out <= '1';
-			else
-				pwm_out <= '0';
-			end if;
-			cout<=cout + 1;
-		else
-			cout<=(others=>'0');
-		end if;
-	end if;
-end process;
-end Behavioral;
+LIBRARY IEEE;
+USE IEEE.STD_LOGIC_1164.ALL;
+USE IEEE.STD_LOGIC_ARITH.ALL;
+USE IEEE.STD_LOGIC_UNSIGNED.ALL;
+ 
+ENTITY pwm IS
+GENERIC (N : INTEGER);
+PORT (
+      clk : IN STD_LOGIC;
+      pwm_count : IN STD_LOGIC_VECTOR(N - 1 DOWNTO 0);
+      pwm_out : OUT STD_LOGIC );
+ 
+END pwm;
+ 
+ARCHITECTURE Behavioral OF pwm IS
+SIGNAL cout : STD_LOGIC_VECTOR(N - 1 DOWNTO 0) := (OTHERS => '0');
+ATTRIBUTE keep : STRING;
+ATTRIBUTE keep OF cout : SIGNAL IS "true";
+BEGIN
+   PROCESS (clk, cout, pwm_count)
+     BEGIN
+      IF falling_edge(clk) THEN
+        IF cout < "11111111" THEN
+           cout <= cout + 1;
+        ELSE
+           cout <= (OTHERS => '0');
+	END IF;
+      END IF;
+   END PROCESS;
+ 
+   PROCESS (clk, cout, pwm_count)
+     BEGIN
+      IF rising_edge(clk) THEN
+	IF cout < pwm_count THEN
+	  pwm_out <= '1';
+	ELSE
+	  pwm_out <= '0';
+	END IF;
+      END IF;
+    END PROCESS;
+ 
+END Behavioral; 
